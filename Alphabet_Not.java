561e351a6076aab79f6da3f2aae70a077cd739d2
yahiya12 <37495588+yahiya12@users.noreply.github.com>
2018-09-24 13:53:36 +0530

    Update Alphabet_Not.java

--- Alphabet_Not.java
+++ Alphabet_Not.java
@@ -4,11 +4,11 @@ public class Alphabet_Not
 {
     public static void main(String args[])
     {
-     char cha;
+     char m;
         Scanner scan = new Scanner(System.in);
-	    System.out.print("Enter a Character : ");
-        cha = scan.next().charAt(0);
-	    if((cha>='a' && cha<='z') || (cha>='A' && cha<='Z'))
+        System.out.print("Enter a Character : ");
+        m = scan.next().charAt(0);
+	    if((m>='a' && m<='z') || (m>='A' && m<='Z'))
         {
             System.out.print( "Alphabet");
         }
