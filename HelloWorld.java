574d9aa2c67d789612d84711ccb096d047ff0030
yahiya12 <37495588+yahiya12@users.noreply.github.com>
2018-09-24 15:41:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
+		for (int n = 0; n < 10; n++) {
 			System.out.println("Hello World");
 		}
 	}
