ea45933fca0dd04a433bf570759ac9a419a80228
yahiya12 <37495588+yahiya12@users.noreply.github.com>
2018-10-05 09:22:40 +0530

    Update ReverseNumberPlayer.java

--- ReverseNumberPlayer.java
+++ ReverseNumberPlayer.java
@@ -6,11 +6,8 @@ class ReverseNumberPlayer
       int num=0;
       int reversenum =0;
       System.out.println("Input your number and press enter: ");
-      //This statement will capture the user input
       Scanner in = new Scanner(System.in);
-      //Captured input would be stored in number num
       num = in.nextInt();
-      //While Loop: Logic to find out the reverse number
       while( num != 0 )
       {
           reversenum = reversenum * 10;
