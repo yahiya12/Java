295b28a2fea94d112cde5cacb1269cb96e81ce93
yahiya12 <37495588+yahiya12@users.noreply.github.com>
2018-10-07 11:09:44 +0530

    Create Vowelreverse.java

---
+++ Vowelreverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class Vowelreverse{
+     public static void main(String arg[]){
+      int a;
+     String y;
+     Scanner s=new Scanner(System.in);
+     y=s.next();
+     String j=h.replaceAll("[AEIOUaeiou]", "");
+     String n=new StringBuilder(j).reverse().toString();
+     System.out.println(n);
+     
+
+    }
+}
